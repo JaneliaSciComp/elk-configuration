57c1141a0e5cc1200301ffbe1e45442e4612e25c
Rob Svirskas <svirskasr@janelia.hhmi.org>
2019-03-11 11:12:34 -0400

    Logstash mods

--- refresh.sh
+++ refresh.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-neuprint_length=`wc -l neuprint.json`
-scp svirskasr@informatics-prod:/groups/flyem/data/proofreading_logs/neuprintlog-emdata1\:11000.json neuprint.json
-neuprint_new=`wc -l neuprint.json`
+neuprint_length=`wc -l /groups/flyem/data/elk_logstash/logs/neuprint.json | awk '{print $1}'`
+cp /groups/flyem/data/proofreading_logs/neuprintlog-emdata1\:11000.json /groups/flyem/data/elk_logstash/logs/neuprint.json
+neuprint_new=`wc -l /groups/flyem/data/elk_logstash/logs/neuprint.json | awk '{print $1}'`
 
-assignmentc_length=`wc -l mad_assignments_completed.json`
-assignments_length=`wc -l mad_assignments_started.json`
-scp svirskasr@informatics-prod:/groups/scicomp/informatics/logs/* .
-assignmentc_new=`wc -l mad_assignments_completed.json`
-assignments_new=`wc -l mad_assignments_started.json`
-
-if [ "$neuprint_new" != "$neuprint_length" ] && [ "$neuprint_new" != 0 ]; then
-    echo "Loading NeuPrint"
-    curl -XDELETE http://localhost:9200/neuprint_index
-    /bin/rm -rf /tmp/logstash_neuprint
-    /opt/logstash/bin/logstash --path.data /tmp/logstash_neuprint -f neuprint_stdin.conf <neuprint.json
-fi
+assignmentc_length=`wc -l /groups/flyem/data/elk_logstash/logs/mad_assignments_completed.json | awk '{print $1}'`
+assignments_length=`wc -l /groups/flyem/data/elk_logstash/logs/mad_assignments_started.json | awk '{print $1}'`
+cp /groups/scicomp/informatics/logs/* /groups/flyem/data/elk_logstash/logs 
+assignmentc_new=`wc -l /groups/flyem/data/elk_logstash/logs/mad_assignments_completed.json | awk '{print $1}'`
+assignments_new=`wc -l /groups/flyem/data/elk_logstash/logs/mad_assignments_started.json | awk '{print $1}'`
 
 if [ "$assignmentc_new" != "$assignmentc_length" ] && [ "$assignmentc_new" != 0 ]; then
-    echo "Loading MAD assignment completions"
-    curl -XDELETE http://localhost:9200/mad_assignment_completed_index
+    echo "Loading MAD assignment completions ($assignmentc_new != $assignmentc_length)"
+    curl -XDELETE http://flyem-elk.int.janelia.org:9200/mad_assignment_completed_index
     /bin/rm -rf /tmp/logstash_mad_assignment_completed
-    /opt/logstash/bin/logstash --path.data /tmp/logstash_mad_assignment_completed -f mad_assignment_completed_stdin.conf <mad_assignments_completed.json
+    /usr/share/logstash/bin/logstash --path.data /tmp/logstash_mad_assignment_completed -f /groups/flyem/data/elk_logstash/mad_assignment_completed_stdin.conf </groups/flyem/data/elk_logstash/logs/mad_assignments_completed.json
 fi
 if [ "$assignments_new" != "$assignments_length" ] && [ "$assignments_new" != 0 ]; then
-    echo "Loading MAD assignment starts"
-    curl -XDELETE http://localhost:9200/mad_assignment_started_index
+    echo "Loading MAD assignment starts ($assignments_new != $assignments_length)"
+    curl -XDELETE http://flyem-elk.int.janelia.org:9200/mad_assignment_started_index
     /bin/rm -rf /tmp/logstash_mad_assignment_started
-    /opt/logstash/bin/logstash --path.data /tmp/logstash_mad_assignment_started -f mad_assignment_started_stdin.conf <mad_assignments_started.json
+    /usr/share/logstash/bin/logstash --path.data /tmp/logstash_mad_assignment_started -f /groups/flyem/data/elk_logstash/mad_assignment_started_stdin.conf </groups/flyem/data/elk_logstash/logs/mad_assignments_started.json
 fi
 
-#/opt/logstash/bin/logstash --path.data /tmp/logstash_edges -f edges.conf
-#/opt/logstash/bin/logstash --path.data /tmp/logstash_false_split -f false_split.conf
+if [ "$neuprint_new" != "$neuprint_length" ] && [ "$neuprint_new" != 0 ]; then
+    echo "Loading NeuPrint ($neuprint_new != $neuprint_length)"
+    curl -XDELETE http://flyem-elk.int.janelia.org:9200/neuprint_index
+    /bin/rm -rf /tmp/logstash_neuprint
+    /usr/share/logstash/bin/logstash --path.data /tmp/logstash_neuprint -f /groups/flyem/data/elk_logstash/neuprint_stdin.conf </groups/flyem/data/elk_logstash/logs/neuprint.json
+fi
